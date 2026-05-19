e32bf6a2d94276cfa542c8aa3082a5d58400967a
advancedadas <adasworkshop123@gmail.com>
2026-05-19 19:53:57 +0930

    Refactor foreign key fields in model associations

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,16 +2,41 @@ import { User } from './User.js';
 import { Job } from './Job.js';
 import { Photo } from './Photo.js';
 
-User.hasMany(Job, { foreignKey: { name: 'assignedToId', allowNull: false }, as: 'assignedJobs' });
-Job.belongsTo(User, { foreignKey: { name: 'assignedToId', allowNull: false }, as: 'assignedTo' });
+User.hasMany(Job, {
+  foreignKey: { name: 'assignedToId', field: 'assigned_to_id', allowNull: false },
+  as: 'assignedJobs'
+});
+Job.belongsTo(User, {
+  foreignKey: { name: 'assignedToId', field: 'assigned_to_id', allowNull: false },
+  as: 'assignedTo'
+});
 
-User.hasMany(Job, { foreignKey: { name: 'createdById', allowNull: false }, as: 'createdJobs' });
-Job.belongsTo(User, { foreignKey: { name: 'createdById', allowNull: false }, as: 'createdBy' });
+User.hasMany(Job, {
+  foreignKey: { name: 'createdById', field: 'created_by_id', allowNull: false },
+  as: 'createdJobs'
+});
+Job.belongsTo(User, {
+  foreignKey: { name: 'createdById', field: 'created_by_id', allowNull: false },
+  as: 'createdBy'
+});
 
-Job.hasMany(Photo, { foreignKey: { name: 'jobId', allowNull: false }, as: 'photos', onDelete: 'CASCADE' });
-Photo.belongsTo(Job, { foreignKey: { name: 'jobId', allowNull: false }, as: 'job' });
+Job.hasMany(Photo, {
+  foreignKey: { name: 'jobId', field: 'job_id', allowNull: false },
+  as: 'photos',
+  onDelete: 'CASCADE'
+});
+Photo.belongsTo(Job, {
+  foreignKey: { name: 'jobId', field: 'job_id', allowNull: false },
+  as: 'job'
+});
 
-User.hasMany(Photo, { foreignKey: { name: 'uploadedById', allowNull: false }, as: 'uploadedPhotos' });
-Photo.belongsTo(User, { foreignKey: { name: 'uploadedById', allowNull: false }, as: 'uploadedBy' });
+User.hasMany(Photo, {
+  foreignKey: { name: 'uploadedById', field: 'uploaded_by_id', allowNull: false },
+  as: 'uploadedPhotos'
+});
+Photo.belongsTo(User, {
+  foreignKey: { name: 'uploadedById', field: 'uploaded_by_id', allowNull: false },
+  as: 'uploadedBy'
+});
 
 export { User, Job, Photo };
